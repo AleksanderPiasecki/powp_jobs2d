687b420022d83352d9a999156d4e141f0352ad51
AleksanderPiasecki <aleksander.piasecki@vp.pl>
2025-12-06 17:56:23 +0100

    add second joe figure to user test

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -24,6 +24,7 @@ public class TestJobs2dPatterns {
 				DriverFeature.getDriverManager());
 
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		application.addTest("Figure Joe 2", selectTestFigureOptionListener);
 	}
 
 	/**
